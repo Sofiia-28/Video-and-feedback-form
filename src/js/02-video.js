f57a2bad9f5fb925c2485760d316ee221df78b79
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-08-26 13:43:44 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = "videoplayer-current-time";
+player.getCurrentTime().then(function(seconds){
+    localStorage.setItem(CURRENT_TIME_KEY, seconds)
+})
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// const currentTime = Number(localStorage.getItem(CURRENT_TIME_KEY)) || 0;
+// player.setCurrentTime(currentTime)
+
+// console.log(timeKey.second);
