213d8456370edcc3ffdd5ce786c0454811627c93
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-08-26 12:31:23 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+
+function createGallery() {
+  const markupLiEl = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`
+    )
+    .join("");
+
+  ulEl.insertAdjacentHTML("beforeend", markupLiEl);
+}
+createGallery();
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
