c7840c27550f04eddc92c55f98445f05ccc777c6
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-08-30 15:30:10 +0300

    edit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,16 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time'
 
 player.setVolume(0);
 
 function getTime(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
 }
 
 player.on('timeupdate', throttle(getTime, 1000));
 
 const currentTime =
-  Number(localStorage.getItem('videoplayer-current-time')) || 0;
+  Number(localStorage.getItem(CURRENT_TIME_KEY));
 player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,33 +6,33 @@ const LOCALSTORAGE_KEY = 'feedback-form-state';
 form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(event) {
-  event.preventDefault();
-  localStorage.setItem(
-    LOCALSTORAGE_KEY,
-    JSON.stringify({
-      email: form.elements.email.value,
-      message: form.elements.message.value,
-    })
-  );
+  let userData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
 }
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
-  console.log({
+  let userData = {
     email: form.elements.email.value,
     message: form.elements.message.value,
-  });
+  };
+  console.log(userData);
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  form.reset();
+  event.target.reset();
 }
 
 window.addEventListener('load', onLoad);
 
-const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
-const parsedData = JSON.parse(savedData);
 function onLoad(event) {
-  form.elements.email.value = parsedData.email;
-  form.elements.message.value = parsedData.message;
+  try {
+    const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+    form.elements.email.value = parsedData.email;
+    form.elements.message.value = parsedData.message;
+  } catch {}
 }
